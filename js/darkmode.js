d6304d8c27469dd7391c3871d9d7239c331c85dd
aishik-pyne <aishikpyne@gmail.com>
2023-09-04 14:42:17 +0000

    This should be final

---
+++ js/darkmode.js
@@ -0,0 +1,6 @@
+console.log("Checking Color mode preference");
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
+}
